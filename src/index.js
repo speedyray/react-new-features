a876b86df90c0dc79118bed4071bb96a4d39f0ae
speedyray <speedyray2ray@gmail.com>
2019-11-01 11:46:31 -0400

    UseState React example initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, {useState}  from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const NotesApp = () => {
+    const [notes, setNotes] = useState([])
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+
+
+
+    const addNote = (e) => {
+        e.preventDefault()
+        setNotes([...notes, { title, body } 
+        ])
+        setTitle('')
+        setBody('')
+      }
+
+      const removeNote = (title) => {
+          setNotes(notes.filter((note) => note.title !== title))
+      }
+
+    return (
+    <div>
+    <h1> Notes</h1> 
+    {notes.map((note) => (
+        <div key={note.title}>
+              <h3> {note.title}</h3>
+              <p>{note.body}</p>
+              <button onClick={() => removeNote(note.title)}>remove Notes</button>    
+        </div>
+    ))}
+
+
+
+    <p> Add note</p>
+    <form onSubmit= { addNote }>
+        <input value={title} onChange={(e) => setTitle(e.target.value)}/>
+       
+        <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+        <button> add note</button>
+    </form>
+            
+    </div>
+    )
+}
+// const App = (props) => {
+//    const [count, setCount] =  useState(props.count)
+//    const [text, setText] = useState('')
+
+//     return (
+//     <div>
+//         <p>The current is {text || 'count'} is {count} </p>
+//         <button onClick={() => setCount(count - 1 )}> -1</button>
+//         <button onClick={() => setCount(count + 1)}> +1</button>
+//         <button onClick={() => setCount(props.count)}> reset</button>
+//         <input value={text} onChange={(e) => setText(e.target.value)}/>
+//     </div>
+//     )
+// }
+
+// App.defaultProps = {
+//     count: 60
+// }
+ReactDOM.render(<NotesApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
